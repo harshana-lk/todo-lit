7fe7dbd941b3396fbc4d8dea696cd197ca0bc6b7
harshana-lk <harshuonlive@gmail.com>
2024-04-03 15:48:05 +0530

    🔖Added Inputs and buttons

--- src/task-manager.ts
+++ src/task-manager.ts
@@ -13,11 +13,96 @@ export class TaskManager extends LitElement {
       color: white;
       font-size: small;
     }
+
+    .button {
+      width: 5em;
+      padding: 1.5em;
+      background-color: black;
+      border: 2px solid white;
+      border-radius: 15px;
+      color: white;
+      font-size: small;
+      cursor: pointer;
+      text-align: center;
+    }
+
+    .taskList {
+      height: 35em;
+      padding: 1.5em;
+      background-color: black;
+      border: 2px solid white;
+      border-radius: 15px;
+      color: white;
+      font-size: small;
+      margin-top: 2em;
+    }
+
+    .task {
+      display: flex;
+      justify-content: space-between;
+      padding: 0.5em;
+      background-color: black;
+      //   border: 2px solid white;
+      border-radius: 15px;
+      color: white;
+      font-size: small;
+      align-items: center;
+      overflow-x: auto;
+      overflow-y: auto;
+    }
+
+    .task-text {
+      width: 40em;
+      padding: 1.5em;
+      background-color: black;
+      //   border: 2px solid white;
+      border-radius: 15px;
+      color: white;
+      font-size: medium;
+      font-weight: bold;
+    }
+
+    .btn-delete {
+      width: 7em;
+      padding: 0.5em;
+      background-color: black;
+      border: 2px solid white;
+      border-radius: 15px;
+      color: white;
+      font-size: small;
+      cursor: pointer;
+      text-align: center;
+      font-weight: bold;
+    }
+    .btn-done {
+      width: 7em;
+      padding: 0.5em;
+      background-color: white;
+      border: 2px solid white;
+      border-radius: 15px;
+      color: black;
+      font-size: small;
+      cursor: pointer;
+      text-align: center;
+      font-weight: bold;
+    }
   `;
   render() {
     return html`
       <div>
-        <input type="text" placeholder="Type here" class="textInput" />
+        <div>
+          <input type="text" placeholder="Type here" class="textInput" />
+          <button class="button">+</button>
+        </div>
+        <div class="taskList">
+          <div class="task">
+            <p class="task-text">This is a task list</p>
+            <div>
+              <button class="btn-done">Done</button>
+              <button class="btn-delete">Delete</button>
+            </div>
+          </div>
+        </div>
       </div>
     `;
   }
